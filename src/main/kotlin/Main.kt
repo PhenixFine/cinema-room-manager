91f60687494a2476ef8acb133a1c3d586d241a50
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-12-27 15:00:45 -0500

    Stage 2 of 5 for JetBrains Academy - Kotlin -  Cinema Room Manager project. • the second stage changed to us calculating income for a theater room dependent on its size ( where if the room has more than 60 seats, the back half is discounted to $8 seat, from the normal $10 a seat ). So stuff from last stage was removed. • main() - previous content removed. Added steps needed to get rows and seats per row to calculate the total income. • getNum(), getString(), and isNumber() are functions I use in various projects and were added for getting numbers from the user.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
 fun main() {
-    val columns = "  1 2 3 4 5 6 7 8"
-    val seats = "S S S S S S S S"
+    val rows = getNum("Enter the number of rows:")
+    val seats = getNum("Enter the number of seats in each row:")
+    val totalSeats = rows * seats
+    val totalIncome = if (totalSeats > 60) rows / 2 * 10 * seats +
+            (rows / 2 + rows % 2) * 8 * seats else totalSeats * 10
 
-    println("Cinema:\n$columns")
-    for (i in 1..7) println("$i $seats")
+    println("Total income:\n$$totalIncome")
 }
+
+fun getNum(text: String, defaultMessage: Boolean = false): Int {
+    val strErrorNum = " was not a number, please try again: "
+    var num = text
+    var default = defaultMessage
+
+    do {
+        num = getString(if (default) num + strErrorNum else num)
+        if (!default) default = true
+    } while (!isNumber(num))
+
+    return num.toInt()
+}
+
+fun getString(text: String): String {
+    println(text)
+    return readLine()!!
+}
+
+fun isNumber(number: String) = number.toIntOrNull() != null
