06ccd3e118eafa6347d66f6fec22545fe2c84745
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-12-28 16:08:56 -0500

    Stage 3 of 5 for JetBrains Academy - Kotlin - Cinema Room Manager project. • the third stage requirements changed to printing the theater room like in stage 1 and then letting the user reserve a seat and printing out the cost of the seat and reprinting the theater with the seat marked as reserved with the letter B. • main() - added an if else statement that verifies that rows and seats isn't less than 1. If it is it prints out the error and then recalls main() for the user to try again. If it's not it sets a 2d array for the theater, calls printSeats() to print it, calls reserveSeat() to reserve the seat, and then calls printSeats() again to print the change. • printSeats() receives the 2d array and prints out the info according to the stage requirements. • reserveSeat() - receives the 2d array and number of seats. Asks the user for the row and seat number to be reserved and then prints out the cost of the seat. If the user enters any number out of bounds for the 2d array a catch statement will print out the error and than recall reserveSeat() for the user to try again.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,17 @@ fun main() {
     val rows = getNum("Enter the number of rows:")
     val seats = getNum("Enter the number of seats in each row:")
     val totalSeats = rows * seats
-    val totalIncome = if (totalSeats > 60) rows / 2 * 10 * seats +
-            (rows / 2 + rows % 2) * 8 * seats else totalSeats * 10
 
-    println("Total income:\n$$totalIncome")
+    if (rows < 1 || seats < 1) {
+        println("please enter positive numbers greater than zero for rows and seats.")
+        main()
+    } else {
+        val theater = Array(rows) { Array(seats) { 'S' } }
+
+        printSeats(theater)
+        reserveSeat(theater, totalSeats)
+        printSeats(theater)
+    }
 }
 
 fun getNum(text: String, defaultMessage: Boolean = false): Int {
@@ -19,6 +26,32 @@ fun getNum(text: String, defaultMessage: Boolean = false): Int {
     } while (!isNumber(num))
 
     return num.toInt()
+}
+
+fun printSeats(theater: Array<Array<Char>>) {
+    print("\nCinema:\n ")
+    for (num in theater[0].indices) print(" ${num + 1}")
+    println()
+
+    for (rowNum in theater.indices) {
+        print(rowNum + 1)
+        for (seat in theater[rowNum]) print(" $seat")
+        println()
+    }
+}
+
+fun reserveSeat(theater: Array<Array<Char>>, totalSeats: Int) {
+    try {
+        val row = getNum("\nEnter a row number:")
+        val seat = getNum("Enter a seat number in that row:")
+        val price = if (totalSeats > 60 && row > theater.size / 2) 8 else 10
+
+        theater[row - 1][seat - 1] = 'B'
+        println("\nTicket price: $$price")
+    } catch (e: IndexOutOfBoundsException) {
+        println("Please enter numbers within 1 and ${theater.size} for rows, and 1 and ${theater[0].size} for seats.")
+        reserveSeat(theater, totalSeats)
+    }
 }
 
 fun getString(text: String): String {
