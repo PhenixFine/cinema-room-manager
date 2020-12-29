c1c7f9edbb22ad67d3827b9adc12e62fa5a9bdf0
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-12-28 20:18:17 -0500

    Stage 5 of 5 for JetBrains Academy - Kotlin - Cinema Room Manager project. • the fifth stage has us keep track of statistics, with using the total income calculated from stage 2 for total possible income, current income, sold seats, and percent of sold seats. It also required that a seat that has already been sold can't be sold again, and to print an error if the user enters numbers out of bounds ( which I already had implemented ). • main() - adjusted to track total income, sold seats, and current income. Added command option for statistics which is passed the values for printing and calculating the percent of sold seats. • menu() - adjusted for the command Statistics • reserveSeat() - adjusted to make sure a seat isn't sold more than once, and message for when the user types a number out of bounds was updated to requirements. • statistics() - calculates the percent of sold seats and formats it to two decimals, and then prints the statistics info according to requirements.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,27 @@
 fun main() {
     val rows = getNum("Enter the number of rows:")
     val seats = getNum("Enter the number of seats in each row:")
-    val totalSeats = rows * seats
 
     if (rows < 1 || seats < 1) {
         println("please enter positive numbers greater than zero for rows and seats.")
         main()
     } else {
         val theater = Array(rows) { Array(seats) { 'S' } }
+        val totalSeats = rows * seats
+        val totalIncome = if (totalSeats > 60) rows / 2 * 10 * seats +
+                (rows / 2 + rows % 2) * 8 * seats else totalSeats * 10
+        var soldSeats = 0
+        var currentIncome = 0
         var command = getNum(menu())
 
         while (command != 0) {
             when (command) {
                 1 -> printSeats(theater)
-                2 -> reserveSeat(theater, totalSeats)
+                2 -> {
+                    currentIncome += reserveSeat(theater, totalSeats)
+                    soldSeats++
+                }
+                3 -> statistics(totalSeats, totalIncome, soldSeats, currentIncome)
             }
             command = getNum(menu())
         }
@@ -34,9 +42,10 @@ fun getNum(text: String, defaultMessage: Boolean = false): Int {
 }
 
 fun menu(): String {
-    return ("\n1. Show the seats\n" +
+    return "\n1. Show the seats\n" +
             "2. Buy a ticket\n" +
-            "0. Exit")
+            "3. Statistics\n" +
+            "0. Exit"
 }
 
 fun printSeats(theater: Array<Array<Char>>) {
@@ -51,18 +60,34 @@ fun printSeats(theater: Array<Array<Char>>) {
     }
 }
 
-fun reserveSeat(theater: Array<Array<Char>>, totalSeats: Int) {
-    try {
+fun reserveSeat(theater: Array<Array<Char>>, totalSeats: Int): Int {
+    return try {
         val row = getNum("\nEnter a row number:")
         val seat = getNum("Enter a seat number in that row:")
         val price = if (totalSeats > 60 && row > theater.size / 2) 8 else 10
 
-        theater[row - 1][seat - 1] = 'B'
-        println("Ticket price: $$price")
+        if (theater[row - 1][seat - 1] == 'B') {
+            println("\nThat ticket has already been purchased!")
+            reserveSeat(theater, totalSeats)
+        } else {
+            theater[row - 1][seat - 1] = 'B'
+            println("\nTicket price: $$price")
+            price
+        }
     } catch (e: IndexOutOfBoundsException) {
-        println("Please enter numbers within 1 and ${theater.size} for rows, and 1 and ${theater[0].size} for seats.")
+        println("\nWrong input!")
         reserveSeat(theater, totalSeats)
     }
+}
+
+fun statistics(totalSeats: Int, totalIncome: Int, soldSeats: Int, currentIncome: Int) {
+    val percent = "%.2f".format(soldSeats.toDouble() / totalSeats * 100)
+    val statString = "\nNumber of purchased tickets: $soldSeats\n" +
+            "Percentage: $percent%\n" +
+            "Current income: $$currentIncome\n" +
+            "Total income: $$totalIncome"
+
+    println(statString)
 }
 
 fun getString(text: String): String {
